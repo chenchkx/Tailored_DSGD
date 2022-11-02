612623a1c64a5552c031cc4933ca4d63013c0fcd
kxchern <chenkx.jsh@aliyun.com>
2022-11-03 02:32:39 +0800

    update alexnet scheme

--- networks/alexnet.py
+++ networks/alexnet.py
@@ -18,11 +18,41 @@ class AlexNet(nn.Module):
         super(AlexNet, self).__init__()
 
 
+        # self.features = nn.Sequential(
+        #     nn.Conv2d(3, 64, kernel_size=11, stride=2, padding=2), # stride: 4->2
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=1), # stride: 2->1
+        #     nn.Conv2d(64, 192, kernel_size=5, padding=2),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        #     nn.Conv2d(192, 384, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(384, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(256, 256, kernel_size=3, padding=1),
+        #     nn.ReLU(inplace=True),
+        #     nn.MaxPool2d(kernel_size=3, stride=2),
+        # )
+        # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(),
+        #     nn.Linear(256 * 6 * 6, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(),
+        #     nn.Linear(4096, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(4096, num_classes),
+        # )
+
+        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        # self.classifier = nn.Linear(256, num_classes)
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=2, padding=2), # stride: 4->2
+            nn.Conv2d(3, 64, kernel_size=11, stride=1, padding=5), # kernel_size: 11->7, stride: 4->1
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=1), # stride: 2->1
+            nn.MaxPool2d(kernel_size=3, stride=2), 
             nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.BatchNorm2d(192),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
@@ -30,19 +60,22 @@ class AlexNet(nn.Module):
             nn.Conv2d(384, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=2),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(4096, 4096),
-            nn.ReLU(inplace=True),
-            nn.Linear(4096, num_classes),
-        )
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Linear(256, num_classes)
+        # self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        # self.classifier = nn.Sequential(
+        #     nn.Dropout(),
+        #     nn.Linear(256 * 6 * 6, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Dropout(),
+        #     nn.Linear(4096, 4096),
+        #     nn.ReLU(inplace=True),
+        #     nn.Linear(4096, num_classes),
+        # )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
 
@@ -69,13 +102,58 @@ def alexnet(pretrained: bool = False, progress: bool = True, **kwargs: Any) -> A
         import copy
         model_dict = copy.deepcopy(model.state_dict())
         for name, param in state_dict.items():
-            if name in ['features.0.weight', 'features.0.bias']:
-                continue
+            # if name in ['features.0.weight', 'features.0.bias']:
+            #     # model_dict[name] = param + torch.std(param)*torch.randn_like(param)
+            #     continue
+            if name in ['features.3.weight']:
+                model_dict['features.4.weight'] =  param
+            elif name in ['features.6.weight']:
+                model_dict['features.8.weight'] =  param
+            elif name in ['features.8.weight']:
+                model_dict['features.10.weight'] =  param
+            elif name in ['features.10.weight']:
+                model_dict['features.12.weight'] =  param
+            elif name in ['features.3.bias']:
+                model_dict['features.4.bias'] =  param
+            elif name in ['features.6.bias']:
+                model_dict['features.8.bias'] =  param
+            elif name in ['features.8.bias']:
+                model_dict['features.10.bias'] =  param
+            elif name in ['features.10.bias']:
+                model_dict['features.12.bias'] =  param
+
+            # if name in ['classifier.1.weight']:
+            #     model_dict['classifier.1.weight'] =  param
+            # elif name in ['classifier.4.weight']:
+            #     model_dict['classifier.4.weight'] =  param
+            # if name in ['classifier.1.bias']:
+            #     model_dict['classifier.1.bias'] =  param
+            # elif name in ['classifier.4.bias']:
+            #     model_dict['classifier.4.bias'] =  param
+            # if name in ['features.3.weight']:
+            #     model_dict['features.4.weight'] =  param
+            # elif name in ['features.6.weight']:
+            #     model_dict['features.8.weight'] =  param
+            # elif name in ['features.8.weight']:
+            #     model_dict['features.10.weight'] =  param
+            # elif name in ['features.10.weight']:
+            #     model_dict['features.12.weight'] =  param
+            # elif name in ['features.3.bias']:
+            #     model_dict['features.4.bias'] =  param
+            # elif name in ['features.6.bias']:
+            #     model_dict['features.8.bias'] =  param
+            # elif name in ['features.8.bias']:
+            #     model_dict['features.10.bias'] =  param
+            # elif name in ['features.10.bias']:
+            #     model_dict['features.12.bias'] =  param
+                # model_dict[name] = param + torch.std(param)*torch.randn_like(param)
                 # model_dict[name] = model_dict[name] + (torch.std(model_dict[name])/torch.std(param))*param
                 # model_dict[name] = state_dict[name] + torch.std(state_dict[name])*torch.randn_like(state_dict[name])
-            elif name in ['classifier.6.weight', 'classifier.6.bias']:
+            # elif name in ['classifier.6.weight', 'classifier.6.bias']:
+            #     continue
+            elif 'classifier' in name:
                 continue
             else:
-                model_dict[name] =  state_dict[name]
+                model_dict[name] =  param
         model.load_state_dict(model_dict)
     return model
--- scripts/tinyimagenet_ring.sh
+++ scripts/tinyimagenet_ring.sh
@@ -8,6 +8,11 @@ python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 64 --m
 python main.py --dataset_name "TinyImageNet" --image_size 64 --batch_size 512 --mode "ring" --size 16  --lr 0.08 --model "AlexNet" --warmup_step 300 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --device 0
 
 
+python main.py --dataset_name "TinyImageNet" --image_size 55 --batch_size 64 --mode "ring" --size 16  --lr 0.04 --model "AlexNet" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --device 0
+python main.py --dataset_name "TinyImageNet" --image_size 55 --batch_size 512 --mode "ring" --size 16  --lr 0.32 --model "AlexNet" --warmup_step 0 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --device 0
+
+
+
 # DenseNet
 python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 64 --mode "ring" --size 16  --lr 0.1 --model "DenseNet" --warmup_step 20 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --device 0
 python main.py --dataset_name "TinyImageNet" --image_size 56 --batch_size 512 --mode "ring" --size 16  --lr 0.8 --model "DenseNet" --warmup_step 20 --milestones 2400 4800 --early_stop 6000 --epoch 6000 --seed 666 --device 0
